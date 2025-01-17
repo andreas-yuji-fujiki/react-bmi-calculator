f3596fe2261a41535222cf8a1e01edf8b86fd624
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 05:29:16 -0300

    created bmi form

---
+++ src/components/molecules/BmiForm.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import FormTitle from "../atoms/FormTitle";
+import FormLabel from "../atoms/FormLabel";
+import FormInput from "../atoms/FormInput";
+import FormSubmitBtn from "../atoms/FormSubmitBtn";
+
+import bmiFormProps from "../../types/BmiFormProps";
+
+export default function BmiForm({ 
+        heightInputValue, 
+        heightInputOnchange,
+
+        weightInputValue,
+        weightInputOnchange
+    } : bmiFormProps 
+) {
+    return(
+        <StyledForm>
+            <FormTitle>
+                BMI Calculator
+            </FormTitle>
+            {/* height input */}
+            <FormGroup>
+                <FormLabel>
+                    Height:
+                </FormLabel>
+                <FormInput 
+                    of="height" 
+                    value={heightInputValue} 
+                    onChange={heightInputOnchange} 
+                />
+            </FormGroup>
+            {/* height input */}
+            <FormGroup>
+                <FormLabel>
+                    Weight:
+                </FormLabel>
+                <FormInput 
+                    of="weight" 
+                    value={weightInputValue} 
+                    onChange={weightInputOnchange} 
+                />
+            </FormGroup>
+            <FormSubmitBtn/>
+        </StyledForm>
+    )
+}
+
+const StyledForm = styled.form``
+const FormGroup = styled.div``

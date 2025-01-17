cc415bbbe69347b333f3fc2906544ed5d4d86b42
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 07:28:08 -0300

    refactoring(result description): updated types

--- src/components/atoms/ResultDescription.tsx
+++ src/components/atoms/ResultDescription.tsx
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 interface descriptionProps {
     description: string
-    color: 'green' | 'yellow' | 'red'
+    descriptionColor: 'green' | 'yellow' | 'red'
 }
 
-export default function ResultDescription( { description, color } : descriptionProps){
+export default function ResultDescription( { description, descriptionColor } : descriptionProps){
     return(
-        <StyledDescription className={color}>
+        <StyledDescription className={descriptionColor}>
             { description }
         </StyledDescription>
     )

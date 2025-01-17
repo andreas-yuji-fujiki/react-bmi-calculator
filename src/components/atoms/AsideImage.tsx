c1045c0107465caec552ffe63a3a636d7aed1be4
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 03:58:11 -0300

    created AsideImage component

---
+++ src/components/atoms/AsideImage.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default function AsideImage(){
+    return(
+        <StyledAsideImage src="/images/aside-image.svg"/>
+    )
+}
+
+const StyledAsideImage = styled.img`
+    width: 40dvw;
+`

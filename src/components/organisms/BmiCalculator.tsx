642837fb53862b785cd250bd7001b8d11e4af4d5
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 07:41:13 -0300

    feat(bmiCalculator): created

--- src/components/organisms/BmiCalculator.tsx
+++ src/components/organisms/BmiCalculator.tsx
@@ -1,10 +1,90 @@
 import styled from "styled-components";
+import { useState } from "react";
+
+import BmiForm from "../molecules/BmiForm";
+import ResultBox from "../molecules/ResultBox";
 
 export default function BmiCalculator(){
+    {/* states */}
+    const [ bmi , setBmi ] = useState<number>(0)
+    const [ description, setDescription ] = useState<string>('')
+    const [ descriptionColor, setDescriptionColor ] = useState< 'green' | 'yellow' | 'red' >('green')
+
+    const [ height , setHeight ] = useState<number>(0)
+    const [ weight , setWeight ] = useState<number>(0)
+
+    {/* calculate bmi function */}
+    function calculateBMI(){
+        if ( height === 0 || height < 0 || weight === 0 || weight < 0) {
+            return
+        }
+
+        const dividedHeight = height / 100
+
+        const calculatedBmi = weight / (dividedHeight * dividedHeight)
+
+        setBmi(calculatedBmi)
+        createResult(calculatedBmi)
+    }
+
+    {/* create description function */}
+    function createResult( calculatedBmi : number){
+        if ( calculatedBmi < 18.5 ) {
+            setDescription('Underweight.')
+            setDescriptionColor('yellow')
+        }
+        if ( calculatedBmi > 18.5 && calculatedBmi < 24.9 ) {
+            setDescription('Normal weight.')
+            setDescriptionColor('green')
+        }
+        if ( calculatedBmi > 24.9 && calculatedBmi < 29.9 ) {
+            setDescription('Overweight.')
+            setDescriptionColor('yellow')
+        }
+        if ( calculatedBmi > 29.9 && calculatedBmi < 34.9 ) {
+            setDescription('Obesity Class 1 (Moderate).')
+            setDescriptionColor('yellow')
+        }
+        if ( calculatedBmi > 34.9 && calculatedBmi < 40) {
+            setDescription('Obesity Class 2 (Severe).')
+            setDescriptionColor('red')
+        }
+        if ( calculatedBmi > 40) {
+            setDescription('Obesity Class 3 (Morbid).')
+            setDescriptionColor('red')
+        }
+    }
+
+    {/* handle submit function */}
+    function handleSubmit( e: React.FormEvent ){
+        e.preventDefault()
+        calculateBMI()
+    }
+    
     return(
         <CalculatorWrapper>
+            {/* form */}
+            <BmiForm
+                heightInputOnchange={(e) => setHeight(Number(e.target.value))}
+                heightInputValue={String(height)}
+
+                weightInputOnchange={(e) => setWeight(Number(e.target.value))}
+                weightInputValue={String(weight)}
+
+                onSubmit={handleSubmit}
+            />
+
+            {/* result */}
+            <ResultWrapper>
+                <ResultBox
+                    bmi={bmi}
+                    description={description}
+                    descriptionColor={descriptionColor}
+                />
+            </ResultWrapper>
         </CalculatorWrapper>
     )
 }
 
 const CalculatorWrapper = styled.div``
+const ResultWrapper = styled.div``

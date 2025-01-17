28e8aba7d445be4bd3c1f76a397207f74fa2eb02
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 05:29:08 -0300

    created bmi props type

---
+++ src/types/BmiFormProps.ts
@@ -0,0 +1,8 @@
+import { ChangeEvent } from "react";
+
+export default interface bmiFormProps {
+    heightInputValue: string;
+    heightInputOnchange: (event : ChangeEvent<HTMLInputElement>) => void;
+    weightInputValue: string;
+    weightInputOnchange: (event : ChangeEvent<HTMLInputElement>) => void;
+}

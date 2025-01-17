880bf54d0c507de751e3bd145daca90d6020e8ee
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 04:38:06 -0300

    created submit button

---
+++ src/components/atoms/FormSubmitBtn.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function FormSubmitBtn(){
+    return(
+        <StyledSubmitBtn type="submit">
+            Calculate BMI
+        </StyledSubmitBtn>
+    )
+}
+
+const StyledSubmitBtn = styled.button``

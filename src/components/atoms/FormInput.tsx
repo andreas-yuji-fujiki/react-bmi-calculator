e1d63f550efb0ef5d2c7c2cb661e120ca9374aeb
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 04:51:45 -0300

    feat: different icons and placeholders for input uses

--- src/components/atoms/FormInput.tsx
+++ src/components/atoms/FormInput.tsx
@@ -1,10 +1,45 @@
 import styled from "styled-components";
 import FormInputProps from '../../types/FormInputProps'
 
-export default function FormInput( { value, onChange } : FormInputProps ){
+import { FaWeight } from "react-icons/fa";
+import { GiBodyHeight } from "react-icons/gi";
+
+export default function FormInput( { value, onChange, of } : FormInputProps ){
     return(
-        <StyledFormInput type="number" value={value} onChange={onChange}/>
+        <>
+            {of === 'height' ?
+                <InputWrapper>
+                    {/* icon */}
+                    <IconWrapper>
+                        <GiBodyHeight/>
+                    </IconWrapper>
+                    {/* input */}
+                    <StyledFormInput
+                        placeholder="Enter your height in meters" 
+                        type="number" 
+                        value={value} 
+                        onChange={onChange}
+                    />
+                </InputWrapper>
+            :
+                <InputWrapper>
+                    {/* icon */}
+                    <IconWrapper>
+                        <FaWeight />
+                    </IconWrapper>
+                    {/* input */}
+                    <StyledFormInput 
+                        placeholder="Enter your weight in kilograms"
+                        type="number" 
+                        value={value} 
+                        onChange={onChange}
+                    />
+                </InputWrapper>
+            };
+        </>
     );
 };
 
+const InputWrapper = styled.div``
+const IconWrapper = styled.div``
 const StyledFormInput = styled.input``
--- src/types/FormInputProps.ts
+++ src/types/FormInputProps.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent } from "react";
 export default interface FormInputProps {
     value: string;
-    for: 'weight' | 'height';
+    of: 'weight' | 'height';
     onChange: (event : ChangeEvent<HTMLInputElement>) => void;
 };

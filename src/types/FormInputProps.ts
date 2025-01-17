f9bc0897d6de9ec4a6d1973798070e41d062bd98
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-01-17 04:39:51 -0300

    updated Form types

--- src/types/FormInputProps.ts
+++ src/types/FormInputProps.ts
@@ -1,5 +1,6 @@
 import { ChangeEvent } from "react";
 export default interface FormInputProps {
     value: string;
+    for: 'weight' | 'height';
     onChange: (event : ChangeEvent<HTMLInputElement>) => void;
 };
